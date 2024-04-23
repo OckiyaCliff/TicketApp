e9846198284e4d6b4abff2724cec6c2d03e45904
Cliff <Ockiyacliffc@gmail.com>
2024-04-23 17:43:59 +0100

    database setup

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import prisma from '@/prisma/db'
 
-const Tickets = () => {
+const Tickets = async () => {
+
+  const Tickets = await prisma.ticket.findMany()
+
+
+  console.log(Tickets)
   return (
     <div>Tickets</div>
   )
